b3efe685dbf9cd2550fccde62dffabf5deefdfcd
Amanda <babygodzilla2010@gmail.com>
2026-01-14 13:49:32 -0500

    Disable TEST_SPARKLES_ANIMATION flag in testConfig.ts

--- src/lib/games/axiomata/testConfig.ts
+++ src/lib/games/axiomata/testConfig.ts
@@ -2,5 +2,5 @@
 // - Marks all difficulties as completed
 // - Always shows sparkles animation
 // - Enables sharing in test mode
-export const TEST_SPARKLES_ANIMATION = true;
+export const TEST_SPARKLES_ANIMATION = false;
 
